4b4d20e520403dd46c8592747745c2a977c2746c
gopinathyaa <mohanrairajaram1@gmail.com>
2024-08-29 13:11:57 +0530

    first

--- src/main/java/com/example/demo/JenkinsApplication.java
+++ src/main/java/com/example/demo/JenkinsApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+ 
+ 
 
 @SpringBootApplication(scanBasePackages = {"com.*"})
-@EntityScan(basePackages = {"com.example.entity"})
-@EnableJpaRepositories (basePackages = {"com.*"})
+ 
+ 
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
